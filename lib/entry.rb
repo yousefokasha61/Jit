f416ff900a4643c60aac7e622b1b06553dc4deac
Yousef Okasha <yousefokasha61@gmail.com>
2023-12-02 21:32:36 +0200

    finish growing trees

---
+++ lib/entry.rb
@@ -0,0 +1,26 @@
+class Entry
+  attr_reader :name, :oid
+
+  REGULAR_MODE = "100644"
+  EXECUTABLE_MODE = "100755"
+  DIRECTORY_MODE = "40000"
+
+  def initialize(name, oid, stat)
+    @name = name
+    @oid = oid
+    @stat = stat
+  end
+
+  def parent_directories
+    @name.descend.to_a[0..-2]
+  end
+
+  def basename
+    @name.basename
+  end
+
+  def mode
+    @stat.executable? ? EXECUTABLE_MODE : REGULAR_MODE
+  end
+
+end
